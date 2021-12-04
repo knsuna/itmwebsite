5866bc97c24ddc19e5ec7bef8f4b315a69043091
knsuna <70191323+knsuna@users.noreply.github.com>
2021-12-03 15:25:28 -1000

    u

--- FinalProject/server.js
+++ FinalProject/server.js
@@ -3,8 +3,10 @@ var app = express();
 var myParser = require("body-parser");
 var mysql = require('mysql');
 var fs = require('fs'); 
-var user_data_filename = `./public/user_data.json`;
+const cookieParser = require('cookie-parser');
+var employee_user_data_filename = `./public/employee_user_data.json`;
 var mgr_user_data_filename = `./public/mgr_user_data.json`;
+var user_data_filename = `./public/cust_user_data.json`;
 
 
 console.log("Connecting to localhost...");
@@ -378,6 +380,168 @@ function create_order(POST, response) {
 
 }
 
+app.get("/login", function (request, response) {
+  username = request.cookies.username
+  response.cookie(`username`, username, {maxAge: 300000});
+  console.log(request.cookies.username)
+  if (typeof user_reg_data[username] != `undefined`) {
+      fullname = user_reg_data[username].name;
+      var contents = fs.readFileSync('./public/order.view', 'utf8'); //So that the display_invoice_table_rows will be rendered with invoice.view
+      return response.send(eval('`' + contents + '`')); // render template string)
+  }
+  return response.redirect(`./login.html`)
+});
+
+app.get("/logout", function (request, response) {
+  response.clearCookie("username").redirect(`./index.html`)
+});
+
+if (fs.existsSync(user_data_filename)) {
+  data = fs.readFileSync(user_data_filename, 'utf-8');
+  stats = fs.statSync(user_data_filename)
+  var user_reg_data = JSON.parse(data); // Takes a string and converts it into object or array    
+  console.log(user_data_filename + ' has ' + stats.size + ' characters');
+
+  console.log(user_reg_data);//Displays register users in user_data.json
+} else {
+  console.log(user_data_filename + ' does not exist!');//Displays warning if user_data.json is missing
+}
+
+app.use(cookieParser());
+
+app.post("/loginform", function (request, response) {
+  // Process login form POST and redirect to logged in page if ok, back to login page if not
+  console.log(request.body);
+  username = request.body.username.toLowerCase();//will recieve username in lowercase ONLY
+  if (typeof user_reg_data[username] != 'undefined') {
+      //if username exists, get password 
+      if ((user_reg_data[username].password == request.body.password) == true) {
+          console.log(username + ' logged in');
+          fullname = user_reg_data[username].name;
+          response.cookie(`username`, username, {maxAge: 300000});
+          console.log(username)
+          response.redirect(`./index.html`)
+      } else {
+          response.send(`<script>
+          alert("The password that you have entered is not correct."); 
+          window.history.back(); 
+          
+          </script>`);
+      }
+  } else {
+      //Option to go back and retry login, or register a new account
+      response.send(`<script>
+      alert("The username you have entered does not match our records."); 
+      window.history.back(); 
+      
+      </script>`);
+  }
+
+});
+//The GET /register is taken from register.html
+app.get("/register", function (request, response) {
+  response.redirect(`./register.html`); // render template string
+});
+//When the POST /register is recieved it will begin the process to register new account
+app.post("/register", function (request, response) {
+  var username = request.body.username;//Assigns username to variable
+  var password = request.body.password;//Assigns password to variable
+  var second_password = request.body.secondpassword;//Assigns second password to variable
+  var email = request.body.email.toLowerCase();//Assigns email to variable and converts to only lowercase
+  var fullname = request.body.fullname;
+
+  /*NOTE: I made the validation this way beucase I wanted to be able to display the particular error messages for each issue. 
+  Instead of creating some type of function, I wanted the user to be able to see exactly what they were doing wrong by display the issue.
+  */
+  //if fullname is greater than 30, display error message
+  if (!validatefullname(fullname)) {
+      console.log(`Full name is more than 30 characters long`);
+      response.send(`<script>
+          alert("${fullname} is not valid. Please make sure it contains only letters and is shorter than 30 characters"); 
+          window.history.back(); 
+          
+          </script>`);
+  }
+  else {
+      var GoodName = true;
+  }
+  //if username is already defined, display error message
+  if ((typeof user_reg_data[username] != 'undefined')) {
+      console.log(`The username requested already exists`);
+      response.send(`<script>
+          alert("${username} already exists."); 
+          window.history.back(); 
+          
+          </script>`);
+  }
+  else {
+      var GoodUsername = true;
+      //if username does not meet requirements from validate Username function (using regular expressions to check)
+  }
+  if (!validateUsername(username)) {
+      console.log(`The username is to long or contains characters other than numbers and letters.`);
+      response.send(`<script>
+          alert("Your username:${username} is invalid. Please make sure that your password is between 4 and 10 characters (currently at ${username.length}) and only contains number and letters."); 
+          window.history.back(); 
+          
+          </script>`);
+  }
+  else {
+      var GoodUsernameLength = true;
+  }
+  //if password is not the same as second password, display error message
+  if (password != second_password) {
+      console.log(`Passwords do not match`);
+      response.send(`<script>
+          alert("Your passwords ${password} and ${second_password} do not match."); 
+          window.history.back(); 
+          
+          </script>`);
+  }
+  else {
+      var GoodPassword = true;
+  }
+  //if password is less than 6, display error message
+  if (password.length < 6) {
+      console.log(`Password is smaller than 6 characters`)
+      response.send(`<script>
+          alert("Your password ${password} is smaller than 6 characters."); 
+          window.history.back(); 
+          
+          </script>`)
+  }
+  else {
+      var GoodLength = true;
+  }
+  //if email does not meet requriments in validate email function, displa error message
+  if (!validateEmail(email)) {
+      console.log(`Email is invalid. Email must only contain letters, numbers, "_", and ".". Domains must be only three characters long.`)
+      response.send(`<script>
+          alert("Your email ${email} is not valid."); 
+          window.history.back(); 
+          
+          </script>`)
+  }
+  else {
+      var GoodEmail = true;
+  }
+  //Checks if every variable is true
+  if (GoodUsername && GoodPassword && GoodLength && GoodEmail && GoodUsernameLength && GoodName) {
+      console.log(`Valid registration`)
+      username = request.body.username.toLowerCase(); //get username in lowercase
+      user_reg_data[username] = {}; //create empty object for array with username
+      user_reg_data[username].name = request.body.fullname; //Assigns full name to new object
+      user_reg_data[username].password = request.body.password; //Assigns password to new object
+      user_reg_data[username].email = request.body.email.toLowerCase(); //Assigns email to new object
+      response.cookie(`username`, username, {maxAge: 300000});
+
+      fs.writeFileSync(user_data_filename, JSON.stringify(user_reg_data)); //This will turn ___ into a string
+
+      response.redirect(`./index.html`)
+  }
+
+});
+
 app.all('*', function (request, response, next) {
   console.log(request.method + ' to ' + request.path);
   next();
@@ -446,26 +610,26 @@ app.post("/user_data", function (request, response) {
 
 
 //Taken from Lab14. Checks to see if user_data.json exists
-if (fs.existsSync(user_data_filename)) {
-  data = fs.readFileSync(user_data_filename, 'utf-8');
-  stats = fs.statSync(user_data_filename)
-  var user_reg_data = JSON.parse(data); // Takes a string and converts it into object or array    
-  console.log(user_data_filename + ' has ' + stats.size + ' characters');
+if (fs.existsSync(employee_user_data_filename)) {
+  data = fs.readFileSync(employee_user_data_filename, 'utf-8');
+  stats = fs.statSync(employee_user_data_filename)
+  var employee_user_reg_data = JSON.parse(data); // Takes a string and converts it into object or array    
+  console.log(employee_user_data_filename + ' has ' + stats.size + ' characters');
 
-  console.log(user_reg_data);//Displays register users in user_data.json
+  console.log(employee_user_reg_data);//Displays register users in user_data.json
 } else {
-  console.log(user_data_filename + ' does not exist!');//Displays warning if user_data.json is missing
+  console.log(employee_user_data_filename + ' does not exist!');//Displays warning if user_data.json is missing
 }
 
-app.post("/loginform", function (request, response) {
+app.post("/employeelogin", function (request, response) {
   // Process login form POST and redirect to logged in page if ok, back to login page if not
   console.log(request.body);
   username = request.body.username.toLowerCase();//will recieve username in lowercase ONLY
-  if (typeof user_reg_data[username] != 'undefined') {
+  if (typeof employee_user_reg_data[username] != 'undefined') {
       //if username exists, get password 
-      if ((user_reg_data[username].password == request.body.password) == true) {
+      if ((employee_user_reg_data[username].password == request.body.password) == true) {
           console.log(username + ' logged in');
-          fullname = user_reg_data[username].name;
+          fullname = employee_user_reg_data[username].name;
           response.cookie(`username`, username, {maxAge: 300000});
           response.redirect(`./employee.html`)
       } else {
